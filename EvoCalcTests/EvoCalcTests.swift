08d4ff812db147b48c535aa77f57fe7a882872ac
Florian schweizer <post.an.flo@googlemail.com>
2016-08-02 20:42:11 +0200

    Initial Commit

---
+++ EvoCalcTests/EvoCalcTests.swift
@@ -0,0 +1,36 @@
+//
+//  EvoCalcTests.swift
+//  EvoCalcTests
+//
+//  Created by florian schweizer on 02.08.16.
+//  Copyright © 2016 florian schweizer. All rights reserved.
+//
+
+import XCTest
+@testable import EvoCalc
+
+class EvoCalcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
